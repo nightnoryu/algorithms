66eba9532a3afc8d7538088337f615496a5e7f4b
m3tro1d <qazwf734@gmail.com>
2021-09-12 20:27:30 +0300

    lab1: add error tests

--- lab1/test.cmd
+++ lab1/test.cmd
@@ -9,25 +9,25 @@ if %SUBJECT%=="" (
 
 
 rem Simple text is processed correctly
-powershell (Measure-Command { %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 2 }).ToString() || goto error
+%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 2 > nul || goto error
 fc "%TEMP%\output.txt" tests\lorem_output.txt > nul || goto error
 echo Test 1 passed
 echo.
 
 rem Simple text with variable line width is processed correctly
-powershell (Measure-Command { %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 40 2 }).ToString() || goto error
+%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 40 2 > nul || goto error
 fc "%TEMP%\output.txt" tests\lorem40_output.txt > nul || goto error
 echo Test 2 passed
 echo.
 
 rem Simple text with variable first line indent is processed correctly
-powershell (Measure-Command { %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 4 }).ToString() || goto error
+%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 4 > nul || goto error
 fc "%TEMP%\output.txt" tests\lorem4ind_output.txt > nul || goto error
 echo Test 3 passed
 echo.
 
 rem Cyrillic text is processed correctly
-powershell (Measure-Command { %SUBJECT% tests\cyrillic.txt "%TEMP%\output.txt" 70 2 }).ToString() || goto error
+%SUBJECT% tests\cyrillic.txt "%TEMP%\output.txt" 70 2 > nul || goto error
 fc "%TEMP%\output.txt" tests\cyrillic_output.txt > nul || goto error
 echo Test 4 passed
 echo.
@@ -37,6 +37,27 @@ powershell (Measure-Command { %SUBJECT% tests\heavy.txt "%TEMP%\output.txt" 70 2
 fc "%TEMP%\output.txt" tests\heavy_output.txt > nul || goto error
 echo Test 5 passed
 echo.
+
+rem Non-existing input file yields an error
+%SUBJECT% non-existing.txt "%TEMP%\output.txt" 70 2 1> nul 2> nul && goto error
+echo Test 6 passed
+echo.
+
+rem Output file on non-existing drive yields an error
+%SUBJECT% tests\lorem.txt "O:\output.txt" 70 2 1> nul 2> nul && goto error
+echo Test 7 passed
+echo.
+
+rem Invalid width value yields an error
+%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" sneed 2 1> nul 2> nul && goto error
+echo Test 8 passed
+echo.
+
+rem Invalid first line indent value yields an error
+%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 sneed 1> nul 2> nul && goto error
+echo Test 9 passed
+echo.
+
 
 rem All tests have passed
 exit /b 0
