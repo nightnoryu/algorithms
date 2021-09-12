e9507296b182e6aea1082ce7a45222a4ff0a47ab
m3tro1d <qazwf734@gmail.com>
2021-09-12 10:45:02 +0300

    lab1: add tests
    1. Add heavy (~1000 lines) test case
    2. Add test timing

--- lab1/test.cmd
+++ lab1/test.cmd
@@ -9,25 +9,34 @@ if %SUBJECT%=="" (
 
 
 rem Simple text is processed correctly
-%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 2 > nul || goto error
+powershell (Measure-Command { %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 2 }).ToString() || goto error
 fc "%TEMP%\output.txt" tests\lorem_output.txt > nul || goto error
 echo Test 1 passed
+echo.
 
 rem Simple text with variable line width is processed correctly
-%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 40 2 > nul || goto error
+powershell (Measure-Command { %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 40 2 }).ToString() || goto error
 fc "%TEMP%\output.txt" tests\lorem40_output.txt > nul || goto error
 echo Test 2 passed
+echo.
 
 rem Simple text with variable first line indent is processed correctly
-%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 4 > nul || goto error
+powershell (Measure-Command { %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 4 }).ToString() || goto error
 fc "%TEMP%\output.txt" tests\lorem4ind_output.txt > nul || goto error
 echo Test 3 passed
+echo.
 
 rem Cyrillic text is processed correctly
-%SUBJECT% tests\cyrillic.txt "%TEMP%\output.txt" 70 2 > nul || goto error
+powershell (Measure-Command { %SUBJECT% tests\cyrillic.txt "%TEMP%\output.txt" 70 2 }).ToString() || goto error
 fc "%TEMP%\output.txt" tests\cyrillic_output.txt > nul || goto error
 echo Test 4 passed
+echo.
 
+rem Heavy text is processed correctly
+powershell (Measure-Command { %SUBJECT% tests\heavy.txt "%TEMP%\output.txt" 70 2 }).ToString() || goto error
+fc "%TEMP%\output.txt" tests\heavy_output.txt > nul || goto error
+echo Test 5 passed
+echo.
 
 rem All tests have passed
 exit /b 0
