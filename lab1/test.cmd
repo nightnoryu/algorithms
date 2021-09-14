f7d8328a12e45418e50c7329002b9c3dc04adfc0
m3tro1d <qazwf734@gmail.com>
2021-09-14 16:09:25 +0300

    lab1: add tests for small line width

--- lab1/test.cmd
+++ lab1/test.cmd
@@ -38,24 +38,30 @@ fc "%TEMP%\output.txt" tests\heavy_output.txt > nul || goto error
 echo Test 5 passed
 echo.
 
+rem Strange line width is processed correctly
+%SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 5 2 > nul || goto error
+fc "%TEMP%\output.txt" tests\lorem5_output.txt > nul || goto error
+echo Test 6 passed
+echo.
+
 rem Non-existing input file yields an error
 %SUBJECT% non-existing.txt "%TEMP%\output.txt" 70 2 1> nul 2> nul && goto error
-echo Test 6 passed
+echo Test 7 passed
 echo.
 
 rem Output file on non-existing drive yields an error
 %SUBJECT% tests\lorem.txt "O:\output.txt" 70 2 1> nul 2> nul && goto error
-echo Test 7 passed
+echo Test 8 passed
 echo.
 
 rem Invalid width value yields an error
 %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" sneed 2 1> nul 2> nul && goto error
-echo Test 8 passed
+echo Test 9 passed
 echo.
 
 rem Invalid first line indent value yields an error
 %SUBJECT% tests\lorem.txt "%TEMP%\output.txt" 70 sneed 1> nul 2> nul && goto error
-echo Test 9 passed
+echo Test 10 passed
 echo.
 
 
