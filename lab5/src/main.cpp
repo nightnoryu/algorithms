59f54866e561725ea43c451756a7505701fa8259
m3tro1d <qazwf734@gmail.com>
2021-10-30 11:43:30 +0300

    lab5: add template

---
+++ lab5/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * lab5
+ *
+ * TODO: add description
+ *
+ * Выполнил:
+ * Хафизов Булат, ПС-21
+ *
+ * Среда выполнения: cmake, g++
+ * Сборка проекта: в корневой директории выполнить команды:
+ *   cmake -G "MSYS Makefiles" .
+ *   cmake --build .
+ * (вместо последней команды можно просто make)
+ * Убедиться, что присутствует cmake, MinGW, MSYS и компилятор g++.
+ */
+
+#include "common_inc.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "Test" << std::endl;
+}
